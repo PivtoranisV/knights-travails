266066f6ea11937e0ae7734f97805ab3e370cbd6
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-09 13:11:20 +0300

    Added possible moves

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -13,3 +13,15 @@ def knight_moves(start_position, end_position)
 
   puts "Start position: #{start_position}, End position: #{end_position}"
 end
+
+def possible_moves(start_position_x, start_position_y)
+  move1 = [start_position_x + 2, start_position_y + 1]
+  move2 = [start_position_x + 2, start_position_y - 1]
+  move3 = [start_position_x - 2, start_position_y + 1]
+  move4 = [start_position_x - 2, start_position_y - 1]
+  move5 = [start_position_x + 1, start_position_y + 2]
+  move6 = [start_position_x + 1, start_position_y - 2]
+  move7 = [start_position_x - 1, start_position_y + 2]
+  move8 = [start_position_x - 1, start_position_y - 2]
+  [move1, move2, move3, move4, move5, move6, move7, move8]
+end
